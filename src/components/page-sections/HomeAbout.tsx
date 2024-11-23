1454d88c5d3de8f6df5f01f4d8614847e1343bdf
Ellen Shimada <ellenshimada@gmail.com>
2024-11-23 15:07:37 -0800

    Add HomeAbout

---
+++ src/components/page-sections/HomeAbout.tsx
@@ -0,0 +1,50 @@
+import { styled } from "@linaria/react";
+import { marked } from "marked";
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const Title = styled.h2`
+  margin: 0;
+`;
+const Content = styled.div`
+  background-color: rgb(var(--rgb-white));
+  color: rgb(var(--rgb-black));
+  padding: var(--space-2) var(--space-8);
+  border-radius: var(--space-2);
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+  gap: var(--space-2);
+  margin: var(--space-4) auto 0;
+  max-width: var(--screen-960);
+  width: 80%;
+`;
+
+export const HomeAbout = ({
+  data,
+}: {
+  data: {
+    displayTitle: string;
+    displaySubTitle: string;
+    displayDescription: string;
+  };
+}) => {
+  return (
+    <>
+      <Header>
+        <Title>{data.displayTitle}</Title>
+        <p>{data.displaySubTitle}</p>
+      </Header>
+      <Content>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: marked.parse(data.displayDescription),
+          }}
+        ></div>
+      </Content>
+    </>
+  );
+};
