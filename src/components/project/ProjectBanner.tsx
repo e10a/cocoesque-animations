f3bd77bbf9acb5bbbb868ab4481f79aced389d96
Ellen Shimada <ellenshimada@gmail.com>
2024-11-26 15:03:45 -0800

    Update styles for project banner

--- src/components/project/ProjectBanner.tsx
+++ src/components/project/ProjectBanner.tsx
@@ -2,16 +2,20 @@ import { styled } from "@linaria/react";
 
 const Outer = styled.div`
   align-items: center;
+  border-radius: 0 var(--space-2) var(--space-2) 0;
   display: flex;
   height: var(--space-8);
   left: 0px;
   margin-top: var(--space-4);
   padding: var(--space-2);
   position: absolute;
-  background-image: linear-gradient(to right, rgb(168, 85, 247), rgb(236, 72, 153));
-  border-radius: 0 var(--space-2) var(--space-2) 0;
+  top: 0;
+  z-index: 1;
+  background-image: linear-gradient(
+    var(--gradient-rb),
+    var(--gradient-indigo-pink)
+  );
 `;
-//  className="shadow-md bg-gradient-to-r from-purple-500 to-pink-500 z-1 absolute top-0 left-0 mt-4 flex gap-2 py-2 px-2 items-center rounded-r-md"
 const Inner = styled.div`
   color: rgb(var(--rgb-white));
   font-size: var(--text-xs);
@@ -19,7 +23,6 @@ const Inner = styled.div`
   font-weight: var(--text-bold);
   letter-spacing: 1.5px;
 `;
-// className="text-xs text-white font-bold uppercase tracking-wider leading-tight"
 export default function ProjectBanner() {
   return (
     <Outer>
