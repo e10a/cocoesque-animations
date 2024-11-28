e4269fa54bccb154b89da23f7615428de500735d
Ellen Shimada <ellenshimada@gmail.com>
2024-11-28 14:13:42 -0800

    Add logic to scroll to top if logo is clicked on the homepage

--- src/components/navigation/NavBarBrand.tsx
+++ src/components/navigation/NavBarBrand.tsx
@@ -1,9 +1,20 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 export default function NavBarBrand() {
+  const location = useLocation();
+
+  const handleClick = () => {
+    if (location.pathname === "/") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    }
+  };
+
   return (
     <div className="nav-bar__brand">
-      <NavLink to="/">
+      <NavLink to="/" onClick={handleClick}>
         <img
           className="nav-bar__logo"
           src="https://jrynlppcbsquzh8t.public.blob.vercel-storage.com/coco-icon-0jftvJAugPhOkZjuqrTNXOO2AZfcuO.svg"
