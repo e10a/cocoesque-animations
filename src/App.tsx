3607e646494e6c530b162d2f53e3e7ddd8d62a3d
Ellen Shimada <ellenshimada@gmail.com>
2024-11-28 13:38:34 -0800

    Try w/o lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
+import PageHome from "@/pages/PageHome.tsx";
+import PageResume from "@/pages/PageResume.tsx";
 import { Route, Routes } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
 import { Provider as ResponsiveProvider } from "@/context/ResponsiveContext.tsx";
 import { lazy } from "react";
 
-const PageHome = lazy(() => import("@/pages/PageHome.tsx"));
-const PageResume = lazy(() => import("@/pages/PageResume.tsx"));
 const DevTools = lazy(() => import("@/components/DevTools.tsx"));
 
 function App() {
