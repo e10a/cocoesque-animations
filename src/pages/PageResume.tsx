f58adb6f909d2a41ceae5d5082c344076fae6990
Ellen Shimada <ellenshimada@gmail.com>
2025-06-26 17:39:07 -0700

    Update resume

--- src/pages/PageResume.tsx
+++ src/pages/PageResume.tsx
@@ -7,6 +7,146 @@ import { getResume } from "@/services/message.service.js";
 import { Achievement, Resume, TechnicalSkill } from "@/types/ResumeData.ts";
 import { styled } from "@linaria/react";
 
+const externalLinks = [
+  {
+    path: "https://github.com/e10a",
+    label: "GitHub",
+    target: "_blank",
+    icon: "GitHub",
+  },
+  {
+    path: "https://www.linkedin.com/in/ellen-s",
+    label: "LinkedIn",
+    target: "_blank",
+    icon: "LinkedIn",
+  },
+];
+const LinksElement = styled.div`
+  display: flex;
+  gap: 10px;
+  align-items: flex-end;
+  row-gap: 0.5rem;
+`;
+const LinkElement = styled.a`
+  display: flex;
+  column-gap: 1.125rem;
+  justify-content: flex-end;
+  align-items: center;
+  background: white;
+  border-radius: 0.25rem;
+  padding: 0.2rem 0.5rem;
+  opacity: 0.8;
+  position: relative;
+  transition: opacity 300ms ease;
+  border: 1px solid rgba(var(--color-indigo-500) / 1);
+
+  &:hover {
+    opacity: 1;
+  }
+
+  &:before {
+    position: absolute;
+    left: 0;
+    z-index: 0;
+    display: block;
+    content: "";
+    background: rgba(var(--color-indigo-700) / 1);
+    height: 100%;
+    width: auto;
+    aspect-ratio: 1/1;
+    border-radius: 0.25rem;
+  }
+
+  &:after {
+    position: absolute;
+    left: 0;
+    margin-left: 0.625rem;
+    z-index: 1;
+    display: block;
+    content: "";
+    background: white;
+    height: 0.625rem;
+    width: 0.625rem;
+  }
+
+  > * {
+    position: relative;
+    z-index: 3;
+    color: rgba(var(--color-indigo-700) / 1);
+    font-weight: bold;
+
+    &:last-child {
+      color: rgba(var(--color-indigo-500) / 1);
+    }
+  }
+`;
+interface LinkType {
+  path: string;
+  label: string;
+  target: string;
+  icon: string;
+}
+const Link = (link: LinkType) => {
+  return (
+    <LinkElement
+      href={link.path}
+      target={link.target}
+      rel={link.target ? "noopener noreferrer" : ""}
+      title={link.path}
+    >
+      <>
+        {!link.icon && link.label}
+
+        {link.icon == "GitHub" && (
+          <>
+            <svg
+              aria-hidden="true"
+              height="14"
+              viewBox="0 0 16 16"
+              version="1.1"
+              width="14"
+              data-view-component="true"
+              className="octicon octicon-mark-github"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                style={{ fill: "currentColor" }}
+                d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"
+              ></path>
+            </svg>
+
+            <span>e10a</span>
+          </>
+        )}
+
+        {link.icon == "LinkedIn" && (
+          <>
+            <svg
+              viewBox="0 0 24 24"
+              width="16"
+              height="16"
+              x="0"
+              y="0"
+              preserveAspectRatio="xMinYMin meet"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g style={{ fill: "currentColor" }} className="solid-icon">
+                <rect
+                  x="-0.003"
+                  style={{ fill: "none" }}
+                  width="24"
+                  height="24"
+                ></rect>
+                <path d="M20,2h-16c-1.1,0-2,0.9-2,2v16c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2V4C22,2.9,21.1,2,20,2zM8,19h-3v-9h3V19zM6.5,8.8C5.5,8.8,4.7,8,4.7,7s0.8-1.8,1.8-1.8S8.3,6,8.3,7S7.5,8.8,6.5,8.8zM19,19h-3v-4c0-1.4-0.6-2-1.5-2c-1.1,0-1.5,0.8-1.5,2.2V19h-3v-9h2.9v1.1c0.5-0.7,1.4-1.3,2.6-1.3c2.3,0,3.5,1.1,3.5,3.7V19z"></path>
+              </g>
+            </svg>
+            <span>ellen-s</span>
+          </>
+        )}
+      </>
+    </LinkElement>
+  );
+};
 const NavContainer = styled.div`
   position: fixed;
   top: 0;
@@ -24,6 +164,27 @@ const NavContainerInset = styled.div`
     var(--gradient-rb),
     var(--gradient-indigo-pink)
   );
+`;
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: start;
+
+  > div {
+    display: flex;
+
+    &:first-child {
+      justify-content: start;
+      align-items: center;
+      font-size: 1.875rem;
+      column-gap: 0.625rem;
+      row-gap: 0;
+
+      > * {
+        margin: 0;
+      }
+    }
+  }
 `;
 const Page = styled.section`
   background-color: rgb(var(--color-white));
@@ -176,10 +337,18 @@ export default function PageResume() {
               </header>
 
               <Page>
-                <header className="flex flex-col gap-1">
-                  <h2>{full_name}</h2>
-                  <p className="tagline">{tag_line}</p>
-                </header>
+                <Header className="flex flex-col gap-1">
+                  <div className="flex">
+                    <h2>{full_name}</h2> <span>|</span>{" "}
+                    <span className="tagline">{tag_line}</span>
+                  </div>
+
+                  <LinksElement>
+                    {externalLinks.map((link: LinkType) => (
+                      <Link key={Math.random()} {...link} />
+                    ))}
+                  </LinksElement>
+                </Header>
 
                 <section id="summary">
                   <h3>Summary</h3>
@@ -229,4 +398,4 @@ export default function PageResume() {
       </PageLayout>
     </>
   );
-}; 
+}
--- src/services/message.service.js
+++ src/services/message.service.js
@@ -1,228 +1,229 @@
 export const getResume = async () => {
-    return {
-      error: "",
-      data: {
-        full_name: "Ellen Kohler",
-        tag_line: "UX Engineer",
-        about:
-          "Seasoned engineer with over 10 years of experience specializing in delivering high-impact features from ideation to implementation. Proven ability to lead projects end-to-end, mentor peers, and collaborate across teams to drive results. Experiences in creating scalable solutions that solve real user needs, with a strong focus on product outcomes and data-driven decision-making. Excited to contribute by owning technical and design challenges, fostering a culture of collaboration, and driving meaningful product improvements.",
-        contact: {
-          phone_number: "949-697-2186",
-          email_address: "ellenshimada@gmail.com",
-        },
-        skills: {
-          linked_in_skills: [
-            {
-              id: 1,
-              name: "Usability",
-              level: 3,
-            },
-            {
-              id: 2,
-              name: "Visual Design",
-              level: 3,
-            },
-            {
-              id: 3,
-              name: "Prototyping",
-              level: 3,
-            },
-            {
-              id: 4,
-              name: "Figma (Software)",
-              level: 3,
-            },
-            {
-              id: 5,
-              name: "User Experience (UX)",
-              level: 3,
-            },
-            {
-              id: 6,
-              name: "User Experience Design (UED)",
-              level: 2,
-            },
-            {
-              id: 7,
-              name: "User Interface Design",
-              level: 2,
-            },
-            {
-              id: 8,
-              name: "Design Systems",
-              level: 2,
-            },
-            {
-              id: 9,
-              name: "Product Design",
-              level: 1,
-            },
-            {
-              id: 10,
-              name: "Mental Models",
-              level: 0,
-            },
-          ],
-          experience_skills: [
-            {
-              id: 1,
-              title: "Design",
-              skills: "Figma, Adobe CS",
-            },
-            {
-              id: 2,
-              title: "Front-End",
-              skills:
-                "HTML, CSS, JavaScript, Vue.js, React.js, TailwindCSS, Ajax, Json, Axios",
-            },
-            {
-              id: 3,
-              title: "Back-End",
-              skills:
-                "Laravel, PHP, PHPUnit, mySQL, PostgreSQL, API interfaces, Node, Ruby, Ruby on Rails",
-            },
-            {
-              id: 4,
-              title: "Others",
-              skills: "Git, GitHub, Jira, Sourcetree, AWS, S3, FullStory",
-            },
-          ],
-          technical_skills: [
-            {
-              id: 1,
-              title: "Frontend Languages and Frameworks",
-              description:
-                "JavaScript, React, Vue.js, HTML5, CSS3, Tailwind CSS, SCSS, JSON, Axios",
-            },
-            {
-              id: 2,
-              title: "Backend Languages and Frameworks",
-              description: "Ruby, Ruby on Rails, Laravel, PHP, MySQL, PostgreSQL",
-            },
-            {
-              id: 3,
-              title: "Design Tools",
-              description: "Figma, Adobe Creative Suite, Procreate",
-            },
-            {
-              id: 4,
-              title: "Prototyping & Development",
-              description:
-                "Interactive Prototypes, UI/UX Design, Responsive Design",
-            },
-            {
-              id: 5,
-              title: "Collaboration Tools",
-              description: "Git, GitHub, Jira, Slack, Confluence",
-            },
-            {
-              id: 6,
-              title: "Soft Skills",
-              description:
-                "Cross-Functional Collaboration, Team Leadership, Agile Development, Data-Driven Decision Making",
-            },
-            {
-              id: 7,
-              title: "Other Tools",
-              description:
-                "FullStory, AWS, Heroku, CircleCI, Code Climate, Segment",
-            },
-          ],
-        },
-        education: {
-          college: "University of California, Los Angeles",
-          degree: "Humanities, B.A.",
-        },
-        experiences: [
+  return {
+    error: "",
+    data: {
+      full_name: "Ellen Kohler",
+      tag_line: "UX Engineer",
+      about:
+        "Seasoned engineer with over 10 years of experience specializing in delivering high-impact features from ideation to implementation. Proven ability to lead projects end-to-end, mentor peers, and collaborate across teams to drive results. Experiences in creating scalable solutions that solve real user needs, with a strong focus on product outcomes and data-driven decision-making. Excited to contribute by owning technical and design challenges, fostering a culture of collaboration, and driving meaningful product improvements.",
+      contact: {
+        phone_number: "949-697-2186",
+        email_address: "ellenshimada@gmail.com",
+      },
+      skills: {
+        linked_in_skills: [
           {
             id: 1,
-            title: "UX Engineer Contractor",
-            company: "Milo",
-            location: "San Francisco, CA",
-            years: "2025, February - March",
-            is_remote: true,
-            items: [
-              "Sole developer to re-engineer existing linear onboarding code base to a modularized set of components. Introduced a simple json driven interface for non-developers to have the ability to dictate modifications, re-ordering, and addition of new steps based on various step types.",
-              "Adopted existing landing page code progress from another developer to quickly implement the remaining 95% of work needed to deploy the new landing page to production including optimization of visual assets for different screen sizes and pixel densities, cross-reference checks across provided design and design system to identify reusable code and elements, simultaneous inspection across mobile and desktop to ensure cohesive quality and experience of the finished product.",
-              "Thoughtfully introduced code changes without compromising the integrity of the existing codebase.",
-              "Implemented A/B testing for UI/UX experiments, collecting data to guide product decisions and optimize the user journey.",
-              "Developed clear documentation of key information and changes for a smooth hand-off.",
-            ],
+            name: "Usability",
+            level: 3,
           },
           {
             id: 2,
-            title: "Full Stack Engineer • Lead UI/UX Designer",
-            company: "Rocksbox, Inc.",
-            location: "San Francisco, CA",
-            years: "2023 - Present",
-            is_remote: true,
-            items: [
-              "Owned the design and development of the company's internal design system, ensuring consistency across engineering teams and improving development efficiency through reusable components in Figma and React.",
-              "Collaborated cross-functionally with product, design, marketing, and engineering teams to create scalable and accessible web interfaces, directly impacting user experience and product goals.",
-              "Drove outcomes by working closely with product managers to prioritize and ship critical features, leading to quicker turnaround times and higher user satisfaction.",
-              "Implemented A/B testing for UI/UX experiments, collecting data to guide product decisions and optimize the user journey.",
-              "Built dashboards using FullStory, enabling data-driven improvements to user flows and contributing to measurable improvements in engagement and usability.",
-            ],
+            name: "Visual Design",
+            level: 3,
           },
           {
             id: 3,
-            title: "Senior Software Engineer • Lead Designer",
-            years: "2022 - 2023",
-            company: "Amava, Inc.",
-            location: "Menlo Park, CA",
-            is_remote: true,
-            items: [
-              "Led technical projects from concept to implementation, owning UI/UX design and full-stack development of responsive, high-conversion web pages using Vue.js and Tailwind CSS.",
-              "Collaborated across functions with marketing and product teams, aligning technical efforts with branding and growth initiatives.",
-              "Played a pivotal role in a major site redesign, directly contributing to increased customer acquisition and engagement metrics through thoughtful and visually appealing design implementations.",
-            ],
+            name: "Prototyping",
+            level: 3,
           },
           {
             id: 4,
-            title: "Software Engineer",
-            years: "2020 - 2021",
-            company: "Shelterluv, Inc.",
-            location: "Palo Alto, CA",
-            is_remote: true,
-            items: [
-              "Solely responsible for developing marketing and sales applications while contributing to core engineering efforts.",
-              "Architected new features in Vue.js/Nuxt and Laravel, aligning technical decisions with product needs to enhance performance and user experience.",
-              "Partnered with the product team to develop and implement UI/UX features, ensuring alignment with customer needs and company objectives.",
-            ],
-          },
-        ],
-        achievements: [
-          {
-            id: 1,
-            title: "End-to-End Ownership",
-            description:
-              "Successfully led multiple high-impact projects from concept through implementation, resulting in measurable improvements in user experience and product performance.",
-          },
-          {
-            id: 2,
-            title: "Mentorship",
-            description:
-              "Mentored junior and senior engineers, shaping the technical strategy and promoting continuous learning within teams.",
-          },
-          {
-            id: 3,
-            title: "Cross-Functional Collaboration",
-            description:
-              "Demonstrated strong collaboration skills across engineering, design, and product teams to drive feature development and align on key product goals.",
-          },
-          {
-            id: 4,
-            title: "Focus on Impact",
-            description:
-              "Delivered features that resulted in increased conversion rates and user engagement by focusing on real user needs and leveraging data to inform decisions.",
+            name: "Figma (Software)",
+            level: 3,
           },
           {
             id: 5,
-            title: "Curious & Self-Driven Fast Learner",
+            name: "User Experience (UX)",
+            level: 3,
+          },
+          {
+            id: 6,
+            name: "User Experience Design (UED)",
+            level: 2,
+          },
+          {
+            id: 7,
+            name: "User Interface Design",
+            level: 2,
+          },
+          {
+            id: 8,
+            name: "Design Systems",
+            level: 2,
+          },
+          {
+            id: 9,
+            name: "Product Design",
+            level: 1,
+          },
+          {
+            id: 10,
+            name: "Mental Models",
+            level: 0,
+          },
+        ],
+        experience_skills: [
+          {
+            id: 1,
+            title: "Design",
+            skills: "Figma, Adobe CS",
+          },
+          {
+            id: 2,
+            title: "Front-End",
+            skills:
+              "HTML, CSS, JavaScript, Vue.js, React.js, TailwindCSS, Ajax, Json, Axios",
+          },
+          {
+            id: 3,
+            title: "Back-End",
+            skills:
+              "Laravel, PHP, PHPUnit, mySQL, PostgreSQL, API interfaces, Node, Ruby, Ruby on Rails",
+          },
+          {
+            id: 4,
+            title: "Others",
+            skills: "Git, GitHub, Jira, Sourcetree, AWS, S3, FullStory",
+          },
+        ],
+        technical_skills: [
+          {
+            id: 1,
+            title: "Frontend Languages and Frameworks",
             description:
-              "Proven ability to quickly adapt to new technologies and processes. In just a few months, self-taught Ruby to contribute effectively as a Full Stack Engineer at Rocksbox, enhancing both frontend and backend development efforts.",
+              "JavaScript, React, Vue.js, HTML5, CSS3, Tailwind CSS, SCSS, JSON, Axios",
+          },
+          {
+            id: 2,
+            title: "Backend Languages and Frameworks",
+            description: "Ruby, Ruby on Rails, Laravel, PHP, MySQL, PostgreSQL",
+          },
+          {
+            id: 3,
+            title: "Design Tools",
+            description: "Figma, Adobe Creative Suite, Procreate",
+          },
+          {
+            id: 4,
+            title: "Prototyping & Development",
+            description:
+              "Interactive Prototypes, UI/UX Design, Responsive Design",
+          },
+          {
+            id: 5,
+            title: "Collaboration Tools",
+            description:
+              "Git, GitHub, Jira, Slack, Confluence, Asana, Google Suite",
+          },
+          {
+            id: 6,
+            title: "Soft Skills",
+            description:
+              "Cross-Functional Collaboration, Team Leadership, Highly Diplomatic, Deeply Compassionate, Exceptionally Empathic, Excellent Communicator",
+          },
+          {
+            id: 7,
+            title: "Other Tools",
+            description:
+              "FullStory, AWS, Heroku, CircleCI, Code Climate, Segment",
           },
         ],
       },
-    };
+      education: {
+        college: "University of California, Los Angeles",
+        degree: "Humanities, B.A.",
+      },
+      experiences: [
+        {
+          id: 1,
+          title: "UX Engineer Contractor",
+          company: "Milo",
+          location: "San Francisco, CA",
+          years: "2025, February - March",
+          is_remote: true,
+          items: [
+            "Sole developer to re-engineer large monolithic codebase into a modularized set of components for onboarding. Introduced a simple json driven interface for non-developers to have the ability to dictate modifications, re-ordering, and addition of new steps based on various step types.",
+            "Adopted existing landing page code progress from another developer to quickly implement the remaining 95% of work needed to deploy the new landing page to production including optimization of visual assets for different screen sizes and pixel densities, cross-reference checks across provided design and design system to identify reusable code and elements, simultaneous inspection across mobile and desktop to ensure cohesive quality and experience of the finished product.",
+            "Thoughtfully introduced code changes without compromising the integrity of the existing codebase.",
+            "Implemented A/B testing for UI/UX experiments, collecting data to guide product decisions and optimize the user journey.",
+            "Developed clear documentation of key information and changes for a smooth hand-off.",
+          ],
+        },
+        {
+          id: 2,
+          title: "Full Stack Engineer • Lead UI/UX Designer",
+          company: "Rocksbox, Inc.",
+          location: "San Francisco, CA",
+          years: "2023 - Present",
+          is_remote: true,
+          items: [
+            "Owned the design and development of the company's internal design system, ensuring consistency across engineering teams and improving development efficiency through reusable components in Figma and React.",
+            "Collaborated cross-functionally with product, design, marketing, and engineering teams to create scalable and accessible web interfaces, directly impacting user experience and product goals.",
+            "Drove outcomes by working closely with product managers to prioritize and ship critical features, leading to quicker turnaround times and higher user satisfaction.",
+            "Implemented A/B testing for UI/UX experiments, collecting data to guide product decisions and optimize the user journey.",
+            "Built dashboards using FullStory, enabling data-driven improvements to user flows and contributing to measurable improvements in engagement and usability.",
+          ],
+        },
+        {
+          id: 3,
+          title: "Senior Software Engineer • Lead Designer",
+          years: "2022 - 2023",
+          company: "Amava, Inc.",
+          location: "Menlo Park, CA",
+          is_remote: true,
+          items: [
+            "Led technical projects from concept to implementation, owning UI/UX design and full-stack development of responsive, high-conversion web pages using Vue.js and Tailwind CSS.",
+            "Collaborated across functions with marketing and product teams, aligning technical efforts with branding and growth initiatives.",
+            "Played a pivotal role in a major site redesign, directly contributing to increased customer acquisition and engagement metrics through thoughtful and visually appealing design implementations.",
+          ],
+        },
+        {
+          id: 4,
+          title: "Software Engineer",
+          years: "2020 - 2021",
+          company: "Shelterluv, Inc.",
+          location: "Palo Alto, CA",
+          is_remote: true,
+          items: [
+            "Solely responsible for developing marketing and sales applications while contributing to core engineering efforts.",
+            "Architected new features in Vue.js/Nuxt and Laravel, aligning technical decisions with product needs to enhance performance and user experience.",
+            "Partnered with the product team to develop and implement UI/UX features, ensuring alignment with customer needs and company objectives.",
+          ],
+        },
+      ],
+      achievements: [
+        {
+          id: 1,
+          title: "End-to-End Ownership",
+          description:
+            "Successfully led multiple high-impact projects from concept through implementation, resulting in measurable improvements in user experience and product performance.",
+        },
+        {
+          id: 2,
+          title: "Mentorship",
+          description:
+            "Mentored junior and senior engineers, shaping the technical strategy and promoting continuous learning within teams.",
+        },
+        {
+          id: 3,
+          title: "Cross-Functional Collaboration",
+          description:
+            "Demonstrated strong collaboration skills across engineering, design, and product teams to drive feature development and align on key product goals.",
+        },
+        {
+          id: 4,
+          title: "Focus on Impact",
+          description:
+            "Delivered features that resulted in increased conversion rates and user engagement by focusing on real user needs and leveraging data to inform decisions.",
+        },
+        {
+          id: 5,
+          title: "Curious & Self-Driven Fast Learner",
+          description:
+            "Proven ability to quickly adapt to new technologies and processes. In just a few months, self-taught Ruby to contribute effectively as a Full Stack Engineer at Rocksbox, enhancing both frontend and backend development efforts.",
+        },
+      ],
+    },
   };
+};
